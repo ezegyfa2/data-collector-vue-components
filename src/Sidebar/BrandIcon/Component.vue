7b009611dcfec9b3f5f6a186d691fe62632c321f
József Bagota <jozsef.bagota@gmail.com>
2023-04-24 13:33:35 +0200

    creating brandicon component.

---
+++ src/Sidebar/BrandIcon/Component.vue
@@ -0,0 +1,9 @@
+<script>
+    import BrandIcon from '../../../../blue-admin-vue-components/src/Sidebar/BrandIcon/Component.vue'
+    
+    export default {
+        mixins: [
+            BrandIcon,
+        ],
+    }
+</script>
