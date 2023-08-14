78b838abf5c6ba48c771cf41ca3359b71670aa7f
ezegyfa2 <darvasvok@gmail.com>
2023-08-15 00:10:49 +0300

    Refactoring code

--- src/Templates/Layout/layout.js
+++ src/Templates/Layout/layout.js
@@ -7,49 +7,39 @@ export default {
                 "sidebar_sections": "-++sidebar_sections"
             }
         },
+        "success_message_sections": {
+            "type": "blue-admin-success-session-message",
+            "array_data": "success_messages",
+            "data": {
+                "content": "++success_messages"
+            }
+        },
+        "error_message_sections": {
+            "type": "blue-admin-error-session-message",
+            "array_data": "error_messages",
+            "data": {
+                "content": "++error_messages"
+            }
+        },
+        "warning_message_sections": {
+            "type": "blue-admin-warning-session-message",
+            "array_data": "warning_messages",
+            "data": {
+                "content": "++warning_messages"
+            }
+        },
+        "info_message_sections": {
+            "type": "blue-admin-info-session-message",
+            "array_data": "info_messages",
+            "data": {
+                "content": "++info_messages"
+            }
+        },
         "content_sections": [
             {
                 "type": "blue-admin-centered-content",
                 "data": {
-                    "content_sections": [
-                        {
-                            "type": "blue-admin-success-session-message",
-                            "array_data": "success_messages",
-                            "merge_to_parent": true,
-                            "data": {
-                                "content": "-++success_messages"
-                            }
-                        },
-                        {
-                            "type": "blue-admin-error-session-message",
-                            "array_data": "error_messages",
-                            "merge_to_parent": true,
-                            "data": {
-                                "content": "-++error_messages"
-                            }
-                        },
-                        {
-                            "type": "blue-admin-warning-session-message",
-                            "array_data": "warning_messages",
-                            "merge_to_parent": true,
-                            "data": {
-                                "content": "-++warning_messages"
-                            }
-                        },
-                        {
-                            "type": "blue-admin-info-session-message",
-                            "array_data": "info_messages",
-                            "merge_to_parent": true,
-                            "data": {
-                                "content": "-++info_messages"
-                            }
-                        },
-                        {
-                            "array_data": "content_sections",
-                            "merge_to_parent": true,
-                            "direct_paste": true
-                        }
-                    ]
+                    "content_sections": "++content_sections"
                 }
             }
         ]
